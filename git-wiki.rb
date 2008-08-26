c0b58955b581b7bd7aafaa58a80e4b5bc2beab3b
Simon Rozet <simon@rozet.name>
2008-08-26 18:54:45 +0200

    use double-quote to wrap html attributes

--- git-wiki.rb
+++ git-wiki.rb
@@ -77,7 +77,8 @@ configure do
   GitRepository = ENV['GIT_WIKI_REPO'] || File.join(ENV['HOME'], 'wiki')
   PageExtension = '.markdown'
   Homepage = 'Home'
-  set_option :haml, :format => :html4
+  set_option :haml,  :format        => :html4,
+                     :attr_wrapper  => '"'
 
   unless (Page.repo = Git.open(GitRepository) rescue false)
     abort "#{GitRepository}: Not a git repository. Install your wiki with `rake bootstrap`"
@@ -92,7 +93,7 @@ helpers do
   end
 
   def list_item(page)
-    "<a class='page_name' href='/#{page}'>#{page.name.titleize}</a>"
+    '<a class="page_name" href="/%s">%s</a>' % [page, page.name.titleize]
   end
 end
 
